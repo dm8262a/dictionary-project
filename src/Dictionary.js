421f7fb0fd7f4c40301dcd77e36062001a64b2e1
dm8262a <114957367+dm8262a@users.noreply.github.com>
2023-08-08 08:05:03 -0400

    update pexels authorization api key

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -27,7 +27,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "Cz5d2Bsh5HFTZUJVNjOWwhVOVm9jTqSFIDanS4FiOeo5t76QZmjnj6CR";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorization: `${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlepexelsResponse);
   }
 
